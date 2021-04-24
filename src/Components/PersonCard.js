032ae3c850467b1f1b762cdae9458d1a97653cc7
Chris Galera <galera.chris@yahoo.com>
2021-04-23 20:48:30 -0500

    Big-Inversion

---
+++ src/Components/PersonCard.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const PersonCard = (props) => {
+    const { lastName, firstName, age, hairColor} = props;
+    return(
+        <div className="container">
+            <h2>{ lastName }, { firstName }</h2>
+            <p>Age: { age }</p>
+            <p>Hair Color: { hairColor }</p>
+        </div>
+    );
+}
+export default PersonCard;
+
